31450b3d0b3af1ab685b033d915c90a55cf0bfd8
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2016-10-18 14:27:53 +0100

    line simulation working
    the periodicity seems to work for the gaussian simulator
    the parameters need to be adjusted

--- R/line_simulation.R
+++ R/line_simulation.R
@@ -1,5 +1,16 @@
 # simulation in one dimension
 
 line_simulation<-function(range,freq,length,seed,N){
+  phase <- runif(N,0,2*pi)
+  bin <- sample(c(-1,1),N,replace=T)
+  w <- rnorm(N, mean = freq , sd = sqrt(2)/range)
+  dist<-(1:100)/100.
+  sim<-numeric(100)
+  for(i in 1:N){
+    sim = sim + cos(bin[i]*w[i]*dist+phase[i])
+  }
+  sim = sim *sqrt(2/N)
+  plot(dist,sim,type="l")
   
+  return(sim)
 }
